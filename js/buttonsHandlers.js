92c617bd002e5541936a39adb9100463c589a6ee
Your Name <you@example.com>
2021-07-10 00:28:21 +0300

    adding mobile event handlers

--- js/buttonsHandlers.js
+++ js/buttonsHandlers.js
@@ -4,9 +4,12 @@ let dropButton = document.querySelector(".button-drop");
 
 // обработчик старта
 startButton.addEventListener("click", event => { timer.start() });
+startButton.addEventListener("touchstart", event => { timer.start() });
 
 // обработчик паузы
 pauseButton.addEventListener("click", event => { timer.pause() });
+pauseButton.addEventListener("touchstart", event => { timer.pause() });
 
 // обработчик сброса
 dropButton.addEventListener("click", event => { timer.drop() });
+dropButton.addEventListener("touchstart", event => { timer.drop() });
