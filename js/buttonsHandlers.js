6ebb92949ab3b817b90a34422fd089b2dd13cb3f
Your Name <you@example.com>
2021-07-31 02:06:25 +0300

    adding of handler 'pointerdown' - event

--- js/buttonsHandlers.js
+++ js/buttonsHandlers.js
@@ -4,9 +4,12 @@ let dropButton = document.querySelector(".button-drop");
 
 // обработчик старта
 startButton.addEventListener("click", event => { timer.start() });
+startButton.addEventListener("pointerdown", event => { timer.start() });
 
 // обработчик паузы
 pauseButton.addEventListener("click", event => { timer.pause() });
+pauseButton.addEventListener("pointerdown", event => { timer.pause() });
 
 // обработчик сброса
 dropButton.addEventListener("click", event => { timer.drop() });
+dropButton.addEventListener("pointerdown", event => { timer.drop() });
